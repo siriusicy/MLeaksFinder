32b08e76da1d3753606d676ee2d48960c4cc4d67
ChenJie <siriusicy@gmail.com>
2023-03-15 17:24:23 +0800

    增大间隔,防误伤

--- MLeaksFinder/NSObject+MemoryLeak.m
+++ MLeaksFinder/NSObject+MemoryLeak.m
@@ -36,7 +36,7 @@ const void *const kLatestSenderKey = &kLatestSenderKey;
         return NO;
     
     __weak id weakSelf = self;
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         __strong id strongSelf = weakSelf;
         [strongSelf assertNotDealloc];
     });
